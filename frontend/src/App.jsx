24987cdc98f0109128f4a670cb71c4a29016f694
Rora Alem <rora.goitom@gmail.com>
2023-09-18 03:36:47 -0400

    Remove sampleDataForPhotoListItem from  App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,13 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-import PhotoFavButton from './components/PhotoFavButton';
-
 import './App.scss';
+import PhotoList from 'components/PhotoList';
 
-// Define an array of photo data objects
-const sampleDataForPhotoListItems = [
-  {
-    id: "1",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "2",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-  {
-    id: "3",
-    location: {
-      city: "Montreal",
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  },
-];
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
-  const photoItems = sampleDataForPhotoListItems.map((photo) => (
+  {/*const photoItems = sampleDataForPhotoListItems.map((photo) => (
     <div key={photo.id} className="photo-list-item">
       <PhotoListItem
         location={`${photo.location.city}, ${photo.location.country}`}
@@ -53,12 +18,10 @@ const App = () => {
       <PhotoFavButton />
     </div>
   ));``
-
+  */}
   return (
     <div className="App">
-
-      {photoItems}
-
+      <PhotoList />
     </div>
   );
 };
