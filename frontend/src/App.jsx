8dc84fb8848531ea89883e53d53b91b6732b51ee
Rora Alem <rora.goitom@gmail.com>
2023-09-18 18:11:32 -0400

    Create Top Navigation Bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -21,7 +21,7 @@ const App = () => {
   */}
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigationBar />
        <PhotoList />
     </div>
   );
