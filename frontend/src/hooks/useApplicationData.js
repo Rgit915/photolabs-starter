abfdab48392907378577b35f8b7ee6297f09b215
Rora Alem <rora.goitom@gmail.com>
2023-09-22 17:37:46 -0400

    Refactor useApplicationData to use useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,52 +1,111 @@
 // CUSTOM REACT HOOK - manage the state for this application
 
-import { useState } from "react";
+import {  useReducer } from "react";
 
+// Define action types as constants for use in the reducer
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+};
+
+//Reducer function to handle state changes based on actions
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      // Logic for adding a favorited photo
+      return {
+        ...state,
+        favoritedPhotos: [...state.favoritedPhotos,
+           action.photoId],
+      };
+
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      // Logic for removing a favorited photo
+      return {
+        ...state,
+        favoritedPhotos: state.favoritedPhotos.filter((id) => id !== action.photoId),
+      };
+
+    case ACTIONS.SELECT_PHOTO:
+      // Logic  for selecting a photo
+      return {
+        ...state,
+        showModal: true,
+        clickedPhoto: action.photo,
+      };
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      // Logic  for displaying photo details
+      return {
+        ...state,
+         showModal: false,
+        clickedPhoto: null
+      };
+
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+// Custom hook to manage application data state
 const useApplicationData = () => {
 
-  //Initialize a state object
-  const [state, setState] = useState({
+  // Initialize state using useReducer
+  const [state, dispatch] = useReducer(reducer, {
     showModal: false,
     clickedPhoto: null,
-    favoritedPhotos: [], //holds the IDS of favorited photos
+    favoritedPhotos: [], // Holds the IDs of favorited photos
+    // photos:
+    // topics:
   });
 
-  /* Update the state when a photo is selected,
-  *  sets showModal to true and updates clickedPhoto  with selected photo*/
+  // Functions for dispatching actions
+  /* Function to set a photo as selected*/
   const setPhotoSelected = (photo) => {
-    setState(prev => ({ ...prev, showModal: true, clickedPhoto: photo }));
+    dispatch({ type: ACTIONS.SELECT_PHOTO, photo });
   };
 
-  /*Close the photo details modal,
-  * sets showModal to false and clears clickedPhoto property */
+  /*Function to close the photo details modal*/
   const onClosePhotoDetailsModal = () => {
-    setState(prev => ({ ...prev, showModal: false, clickedPhoto: null }));
+    dispatch({ type: ACTIONS.DISPLAY_PHOTO_DETAILS });
   };
 
-  /* Update the list of favorited photos
-    * takes photoId and toggles its presence in favoritePhotos array
-    * If aleardy in the array, removed otherwise it's added */
-
+  /*Function to update the list of favorited photos*/
   const updateToFavPhotoIds = (photoId) => {
-    setState(prev => {
-      const isFavorited = prev.favoritedPhotos.includes(photoId);
-      if (isFavorited) {
-        // Remove from favorites
-        return { ...prev, favoritedPhotos: prev.favoritedPhotos.filter((id) => id !== photoId) };
-      } else {
-        // Add to favorites
-        return { ...prev, favoritedPhotos: [...prev.favoritedPhotos, photoId] };
-      }
+    dispatch({
+      type: state.favoritedPhotos.includes(photoId)
+        ? ACTIONS.FAV_PHOTO_REMOVED
+        : ACTIONS.FAV_PHOTO_ADDED,
+      photoId,
     });
   };
 
-  /*  hook returns an object containing current state
-    * and the three functions to interact with  */
+  // // Function to set photo data
+  // const setPhotoFunction = () => {
+
+  // };
+
+  //  // Function to set photo data
+  //  const setTopicFunction = () => {
+
+  //  };
+
+  // Hook returns an object containing current state and the three functions to interact with
   return {
     state,
     setPhotoSelected,
     updateToFavPhotoIds,
     onClosePhotoDetailsModal,
+    // Add  functions for setting photo and topic data
+    // setPhotoFunction,
+    // setTopicFunction,
   };
 };
 
