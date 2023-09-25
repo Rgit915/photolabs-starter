ee73fefd023a56b7a10a8b29b8d8e86498b86f3e
Rora Alem <rora.goitom@gmail.com>
2023-09-25 18:40:08 -0400

    Remove state, use prop from parent for Favbutton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,21 +3,20 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton({ initialSelected = false , photoId, toggleFavorite}) {
-  const [selected, setSelected] = useState(initialSelected);
 
-  // Toggle the selected state when the icon is clicked
+  // Toggle favorite when the icon is clicked
   const handleIconClick = () => {
     toggleFavorite(photoId);
-    setSelected(!selected);
+
   };
 
   return (
     <div className="photo-list__fav-icon">
       <div
-        className={`photo-list__fav-icon-svg ${selected ? 'active' : 'inactive'}`}
+        className={`photo-list__fav-icon-svg ${initialSelected ? 'active' : 'inactive'}`}
         onClick={handleIconClick}
       >
-        <FavIcon selected={selected} />
+        <FavIcon selected={initialSelected} />
       </div>
     </div>
   );
